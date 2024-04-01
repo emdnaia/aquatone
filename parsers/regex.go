3dc64aa5ad103d8cd64e5046b193d0ad89b68ddf
emdnaia <148504452+emdnaia@users.noreply.github.com>
2024-04-01 06:37:10 +0200

    Update regex.go
    fix bug

--- parsers/regex.go
+++ parsers/regex.go
@@ -1,31 +1,31 @@
 package parsers
 
 import (
-	"bufio"
-	"io"
-
-	"github.com/mvdan/xurls"
+        "bufio"
+        "io"
+        "github.com/mvdan/xurls"
 )
 
 type RegexParser struct{}
 
 func NewRegexParser() *RegexParser {
-	return &RegexParser{}
+        return &RegexParser{}
 }
 
 func (p *RegexParser) Parse(r io.Reader) ([]string, error) {
-	var targets []string
-	targetsFilter := make(map[string]struct{})
+        var targets []string
+        targetsFilter := make(map[string]struct{})
 
-	scanner := bufio.NewScanner(r)
-	for scanner.Scan() {
-		for _, target := range xurls.Relaxed().FindAllString(scanner.Text(), -1) {
-			if _, found := targetsFilter[target]; found {
-				continue
-			}
-			targets = append(targets, target)
-			targetsFilter[target] = struct{}{}
-		}
-	}
-	return targets, nil
+        scanner := bufio.NewScanner(r)
+        for scanner.Scan() {
+                // Corrected usage of xurls.Relaxed as a variable
+                for _, target := range xurls.Relaxed.FindAllString(scanner.Text(), -1) {
+                        if _, found := targetsFilter[target]; found {
+                                continue
+                        }
+                        targets = append(targets, target)
+                        targetsFilter[target] = struct{}{}
+                }
+        }
+        return targets, nil
 }
